452108d36a18dedfbbea672547e7481e0b7921e8
byanezzz <biancayanez89@gmail.com>
2017-11-18 19:14:14 -0300

    grafica achievement con datos reales

--- js/app.js
+++ js/app.js
@@ -9,14 +9,9 @@ $(document).ready(function(){
 /*FIN Código barra lateral*/
 
 /*INICIO Código dasshboard*/
-google.charts.load('current', {'packages':['bar']});
-
- 
-google.charts.load('current', {'packages':['corechart']});
-google.charts.setOnLoadCallback(techSkillCakeDraw);
-
-google.charts.load('current', {'packages':['corechart']});
-google.charts.setOnLoadCallback(achievementDraw);
+google.charts.load('current', {'packages':['bar']}); //Barra
+google.charts.load("current", {packages:["corechart"]});//Torta, Histograma
+google.charts.load('current', {'packages':['corechart']});//Curvas
 
 function techSkillCakeDraw() {
     var data = new google.visualization.DataTable();
@@ -33,38 +28,45 @@ function techSkillCakeDraw() {
                        colors: ['#f9a91a','rgb(20, 45, 60)']};
 
      var chart = new google.visualization.PieChart(document.getElementById('cakeTech'));
-     var chart2 = new google.visualization.PieChart(document.getElementById('cakeHse'));
-     
     chart.draw(data, options);
-    chart2.draw(data,options);
 }
 
-function enrollmentDraw(data) {
+function HSESkillCakeDraw() {
+  var data = new google.visualization.DataTable();
+  data.addColumn('string', 'Topping');
+  data.addColumn('number', 'Slices');
+  data.addRows([
+  ['Aprobadas', 20],
+  ['Reprobadas',85],
+  ]);
+
+  var options = {title:'Alumnas sobre el 70%. Promedio de todos los Spring',
+                     width:300,
+                     height:300,
+                     colors: ['#f9a91a','rgb(20, 45, 60)']};
+
+  var chart = new google.visualization.PieChart(document.getElementById('cakeHse'));
+  chart.draw(data, options);
+}
+
+function enrollmentDraw(dataG) {
   var data = new google.visualization.DataTable();
   data.addColumn('string','Sprint');
-  data.addColumn('number','Activas');
-  for(var i in dataG1){
+  data.addColumn('number','Active');
+  for(var i in dataG){
     data.addRows([
-      [i,dataG1[i]]          
+      [i,dataG[i]]          
     ]);
   }
-  
-  /*var data = google.visualization.arrayToDataTable(
-  [
-    ['Spring', 'Activas'],
-    ['S1', 1000],
-    ['S2', 1170],
-    ['S3', 660]    
-  ]);*/
-
+   
   var options = {
     chart: {
       title: 'Enrollment',
-      subtitle: 'Sales, Expenses, and Profit: 2014-2017'
+      subtitle: 'Active students per Sprint'
     },
     colors:'#f9a91a',
     series: {
-        0: { axis: 'activas' }, // Bind series 0 to an axis named 'distance'.
+        0: { axis: 'Active' }, 
       },
     axes: {
         y: {
@@ -74,9 +76,9 @@ function enrollmentDraw(data) {
   };
 
   var chart = new google.charts.Bar(document.getElementById('columnchart_material'));
-
   chart.draw(data, google.charts.Bar.convertOptions(options));
 }
+
 var dataG1={};
 function enrollment(data){
   dataG1={};
@@ -97,14 +99,33 @@ function enrollment(data){
   }
 }
 
-function achievementDraw() {
-  var data = google.visualization.arrayToDataTable([
-    ['Year', 'Sales', 'Expenses'],
+function achievementDraw(dataHSE) {
+  var data = new google.visualization.DataTable();
+  data.addColumn('string','Sprint');
+  data.addColumn('number','Score average');
+
+  var sortable = [];
+  for (var i in dataHSE) {
+      sortable.push([i, dataHSE[i]]);
+  }
+  
+  sortable.sort(function(a, b) {
+      return a[1] - b[1];
+  });
+
+  for(var i=0; i<sortable.length;i++){
+    data.addRows([
+      [sortable[i][0],sortable[i][1] ]         
+    ]);
+  }
+  
+  /*var data = google.visualization.arrayToDataTable([
+    ['SPRINT', 'HSE', 'TECH'],
     ['2004',  1000,      400],
     ['2005',  1170,      460],
     ['2006',  660,       1120],
     ['2007',  1030,      540]
-  ]);
+  ]);*/
 
   var options = {
     title: 'Company Performance',
@@ -115,21 +136,53 @@ function achievementDraw() {
 
   var chart = new google.visualization.LineChart(document.getElementById('achievement'));
   chart.draw(data, options);
+}
+var dataG2HSE={};
+function achievement(data){
+dataG2HSE={};
+var sprintStudent;
+var HSEStudents=0;
+var TechStudents=0;
+if(data!==undefined && data.hasOwnProperty("students")){
+  for(var i=0; i<data.students.length;i++){    
+    if(data.students[i].hasOwnProperty("sprints")){
+      sprintStudent=data.students[i].sprints 
+      for(var j=0; j<sprintStudent.length;j++){
+        if ((sprintStudent[j].score.hse+sprintStudent[j].score.tech)>2100){
+          if(dataG2HSE.hasOwnProperty('S'+sprintStudent[j].number)){
+            dataG2HSE['S'+sprintStudent[j].number]=dataG2HSE['S'+sprintStudent[j].number]+1;
+          }else{
+            dataG2HSE['S'+sprintStudent[j].number]=1
+          }
+        }
+       
+      }
+       
+    }
+  }
+}
+}
 
+  function npsDraw() {
+    var data = google.visualization.arrayToDataTable([
+      ['Year', 'Sales', 'Expenses'],
+      ['2004',  1000,      400],
+      ['2005',  1170,      460],
+      ['2006',  660,       1120],
+      ['2007',  1030,      540]
+    ]);
+  
+    var options = {
+      title: 'Company Performance',
+      curveType: 'function',
+      legend: { position: 'bottom' },
+      colors:['#f9a91a','rgb(20, 45, 60)']
+    };
   var chart2 = new google.visualization.LineChart(document.getElementById('nps'));
   chart2.draw(data, options);
-
-  var chart3 = new google.visualization.LineChart(document.getElementById('student_satisfaction'));
-  chart3.draw(data, options);
-
-  var chart4 = new google.visualization.LineChart(document.getElementById('teacher_rating'));
-  chart4.draw(data, options);
-
-  var chart5 = new google.visualization.LineChart(document.getElementById('jedi_rating'));
-  chart5.draw(data, options);
 }
-google.charts.load("current", {packages:["corechart"]});
-google.charts.setOnLoadCallback(drawChart);
+
+
 function drawChart() {
   var data = google.visualization.arrayToDataTable([
     ['Dinosaur', 'Length'],
@@ -197,11 +250,20 @@ for (var i=0; i<dashboardGenerations.length;i++){
     dataSedePeriodo();
   })
 }
+function studentInfo(data){
+
+}
 
 function dataSedePeriodo(){
   dataPeriodo=data[sede][periodo];
+  achievement(dataPeriodo);
   enrollment(dataPeriodo);
-  google.charts.setOnLoadCallback(enrollmentDraw(dataG1));  
+  google.charts.setOnLoadCallback(enrollmentDraw(dataG1));
+  google.charts.setOnLoadCallback(drawChart());
+  google.charts.setOnLoadCallback(techSkillCakeDraw());
+  google.charts.setOnLoadCallback(HSESkillCakeDraw());
+  google.charts.setOnLoadCallback(achievementDraw(dataG2HSE));
+  google.charts.setOnLoadCallback(npsDraw());  
 };
 
 /*FIN Código dashboard*/
